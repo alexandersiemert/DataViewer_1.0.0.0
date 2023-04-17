80781f0eed8081fbbf35564b3cc8860373181e63
alexandersiemert <alexander@siemert.de>
2023-04-17 16:51:40 +0200

    #7 Cursor und Crosshair angepasst

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,7 +25,14 @@ namespace DataViewer_1._0._0._0
     /// </summary>
     public partial class MainWindow : Window
     {
-        HSpan vLine1;
+        ScatterPlot pltAlt, pltTemp, pltAcc;
+
+        Axis yAxisTemp, yAxisAcc;
+        
+        HSpan measuringSpan;
+
+        VLine crosshairX;
+        HLine crosshairAlt, crosshairTemp, crosshairAcc;
 
         public MainWindow()
         {
@@ -40,7 +47,7 @@ namespace DataViewer_1._0._0._0
             WpfPlot1.Plot.Title("SI-TL1");
 
             //Daten für Höhe zum Plot WpfPlot1 (in XAML definiert) hinzufügen
-            var pltAlt = WpfPlot1.Plot.AddScatter(xh, yh);
+            pltAlt = WpfPlot1.Plot.AddScatter(xh, yh);
             pltAlt.YAxisIndex = WpfPlot1.Plot.LeftAxis.AxisIndex;
             WpfPlot1.Plot.YAxis.Label("Altitude [m]");
             pltAlt.MarkerSize = 1;
@@ -48,8 +55,8 @@ namespace DataViewer_1._0._0._0
             WpfPlot1.Plot.YAxis.Color(pltAlt.Color);
 
             //Daten für Temperatur zum Plot WpfPlot1 (in XAML definiert) hinzufügen
-            var pltTemp = WpfPlot1.Plot.AddScatter(xh, yt);
-            var yAxisTemp = WpfPlot1.Plot.AddAxis(Edge.Right);
+            pltTemp = WpfPlot1.Plot.AddScatter(xh, yt);
+            yAxisTemp = WpfPlot1.Plot.AddAxis(Edge.Right);
             pltTemp.YAxisIndex = yAxisTemp.AxisIndex;
             yAxisTemp.Label("Temperature [°C]");
             pltTemp.MarkerSize = 1;
@@ -57,8 +64,8 @@ namespace DataViewer_1._0._0._0
             yAxisTemp.Color(pltTemp.Color);
 
             //Daten für Beschleunigung zum Plot WpfPlot1 (in XAML definiert) hinzufügen
-            var pltAcc = WpfPlot1.Plot.AddScatter(xh, ya);
-            var yAxisAcc = WpfPlot1.Plot.AddAxis(Edge.Right);
+            pltAcc = WpfPlot1.Plot.AddScatter(xh, ya);
+            yAxisAcc = WpfPlot1.Plot.AddAxis(Edge.Right);
             pltAcc.YAxisIndex = yAxisAcc.AxisIndex;
             yAxisAcc.Label("Acceleration [g]");
             pltAcc.MarkerSize = 1;
@@ -141,9 +148,89 @@ namespace DataViewer_1._0._0._0
                 }
             };
            
+            
 */
+           
             WpfPlot1.Refresh();
             
+        }
+
+
+        //-----------------EVENTHANDLER-------------------------------------------
+
+        // EventHandler für das Dragged-Ereignis
+
+        private void crosshairX_Dragged(object sender, EventArgs e)
+        {
+            
+        }
+        private void crosshairAlt_Dragged(object sender, EventArgs e)
+        {
+            var draggedPixel = WpfPlot1.Plot.GetPixelY(crosshairAlt.Y);
+            crosshairTemp.Y = WpfPlot1.Plot.GetCoordinateY(draggedPixel,yAxisTemp.AxisIndex);
+            crosshairAcc.Y = WpfPlot1.Plot.GetCoordinateY(draggedPixel, yAxisAcc.AxisIndex);
+        }
+
+        //------------------BUTTON FUNKTIONEN---------------------------------------
+
+        //Measuring Cursor enable / disable
+        private void toggleButtonMeasuringCursor_Checked(object sender, RoutedEventArgs e)
+        {
+            measuringSpan = WpfPlot1.Plot.AddHorizontalSpan((WpfPlot1.Plot.GetAxisLimits().XMin+((WpfPlot1.Plot.GetAxisLimits().XMax - WpfPlot1.Plot.GetAxisLimits().XMin) / 4)), (WpfPlot1.Plot.GetAxisLimits().XMax - ((WpfPlot1.Plot.GetAxisLimits().XMax - WpfPlot1.Plot.GetAxisLimits().XMin) / 4)));
+            measuringSpan.DragEnabled = true;
+            WpfPlot1.Refresh();
+        }
+        private void toggleButtonMeasuringCursor_Unchecked(object sender, RoutedEventArgs e)
+        {
+            WpfPlot1.Plot.Remove(measuringSpan);
+            WpfPlot1.Refresh();
+        }
+
+        //Crosshair enable / disable
+        private void toggleButtonCrosshair_Checked(object sender, RoutedEventArgs e)
+        {
+            crosshairX = WpfPlot1.Plot.AddVerticalLine(WpfPlot1.Plot.GetAxisLimits().XMin + (WpfPlot1.Plot.GetAxisLimits().XMax - WpfPlot1.Plot.GetAxisLimits().XMin) / 2);
+            crosshairX.PositionLabel = true;
+            crosshairX.PositionLabelBackground = crosshairX.Color;
+            crosshairX.DragEnabled = true;
+
+            crosshairAlt = WpfPlot1.Plot.AddHorizontalLine(WpfPlot1.Plot.GetAxisLimits().YMin + (WpfPlot1.Plot.GetAxisLimits().YMax - WpfPlot1.Plot.GetAxisLimits().YMin) / 2, color: Color.Black);
+            crosshairAlt.YAxisIndex = pltAlt.YAxisIndex;
+            crosshairAlt.PositionLabel = true;
+            crosshairAlt.PositionLabelBackground = crosshairAlt.Color;
+            crosshairAlt.DragEnabled = true;
+
+
+            crosshairTemp = WpfPlot1.Plot.AddHorizontalLine(pltTemp.GetAxisLimits().YMin + (pltTemp.GetAxisLimits().YMax - pltTemp.GetAxisLimits().YMin) / 2, color: Color.Transparent);
+            crosshairTemp.YAxisIndex = pltTemp.YAxisIndex;
+            crosshairTemp.PositionLabel = true;
+            crosshairTemp.PositionLabelOppositeAxis = true;
+            crosshairTemp.PositionLabelBackground = Color.Red;
+            crosshairTemp.PositionLabelAxis = yAxisTemp;
+            crosshairTemp.DragEnabled = true;
+
+            crosshairAcc = WpfPlot1.Plot.AddHorizontalLine(pltAcc.GetAxisLimits().YMin + (pltAcc.GetAxisLimits().YMax - pltAcc.GetAxisLimits().YMin) / 2, color: Color.Transparent);
+            crosshairAcc.YAxisIndex = pltAcc.YAxisIndex;
+            crosshairAcc.PositionLabel = true;
+            crosshairAcc.PositionLabelOppositeAxis = true;
+            crosshairAcc.PositionLabelBackground = Color.Green;
+            crosshairAcc.PositionLabelAxis = yAxisAcc;
+            crosshairAcc.DragEnabled = true;
+
+            // Registriere einen EventHandler für das Dragged-Ereignis
+            crosshairX.Dragged += crosshairX_Dragged;
+            crosshairAlt.Dragged += crosshairAlt_Dragged;
+
+            //Plot refreshen
+            WpfPlot1.Refresh();
+        }
+        private void toggleButtonCrosshair_Unchecked(object sender, RoutedEventArgs e)
+        {
+            WpfPlot1.Plot.Remove(crosshairX);
+            WpfPlot1.Plot.Remove(crosshairAlt);
+            WpfPlot1.Plot.Remove(crosshairTemp);
+            WpfPlot1.Plot.Remove(crosshairAcc);
+            WpfPlot1.Refresh();
         }
     }
 }
